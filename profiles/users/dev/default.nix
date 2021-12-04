18b88e60b358d7845639c0e7d90d47931eb45b2c
Th3Whit3Wolf <the.white.wolf.is.1337@gmail.com>
2021-12-04 13:22:07 +0000

    Settings for nix polyglot

--- profiles/users/dev/default.nix
+++ profiles/users/dev/default.nix
@@ -5,13 +5,43 @@ let
 in
 {
   home = {
+    packages = [ pkgs.nerdfonts ];
+    file.".vscode/extensions/ms-vsliveshare.vsliveshare-1.0.5090" = {
+      source = pkgs.vscode-extensions.ms-vsliveshare.vsliveshare;
+    };
     nix-polyglot = {
       enable = true;
-      langs = [ "all" ];
+      langs = lib.mkForce [ "all" ];
       enableZshIntegration = false;
       neovim.enable = true;
       vscode = {
         enable = true;
+        userSettings = {
+          editor = {
+            fontFamily =
+              "'JetBrainsMono Nerd Font Mono', monospace, 'Droid Sans Fallback'";
+            fontLigatures = true;
+            inlayHints.fontFamily =
+              "'VictorMono Nerd Font Mono', monospace, 'Droid Sans Fallback'";
+          };
+          extensions = {
+            autoCheckUpdates = false;
+            autoUpdate = false;
+          };
+          window.menuBarVisibility = "toggle";
+          workbench = {
+            colorTheme = "Spacemacs - dark";
+            iconTheme = "material-icon-theme";
+          };
+          update = {
+            mode = "none";
+            showReleaseNotes = false;
+          };
+          telemetry = {
+            enableTelemetry = false;
+            enableCrashReporter = false;
+          };
+        };
         #package = pkgs.vscode;
       };
     };
