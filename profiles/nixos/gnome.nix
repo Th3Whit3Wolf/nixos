c555e375a287021276fcdeb58cc217ff15eec8d3
Th3Whit3Wolf <the.white.wolf.is.1337@gmail.com>
2021-12-04 13:20:21 +0000

    Major changes to gnome profile

--- profiles/nixos/gnome.nix
+++ profiles/nixos/gnome.nix
@@ -1,36 +1,59 @@
-{ config, pkgs, unstable, ... }: {
-config = {
-  environment.systemPackages = with pkgs; [
-    gnome.adwaita-icon-theme
-    gnomeExtensions.appindicator 
+{ config, pkgs, unstable, ... }: 
+let 
+  extensions = with pkgs.gnomeExtensions; [
+    appindicator
+    tweaks-in-system-menu
+    blur-me
+    blur-my-shell
+    just-perfection
+    gnome-40-ui-improvements
+    user-themes
+    big-sur-status-area
+    brightness-control-using-ddcutil
+    tray-icons-reloaded
+    dash-to-dock
+    timepp
   ];
-  programs.dconf.enable = true;
-  # gdm wont find user if `users.users.<user>.shell = pkgs.zsh;` and this isn't set
-  programs.zsh.enable = true;
-  services = {
-    gnome = {
-      gnome-keyring.enable = true;
-      sushi.enable = true;
-    };
-    xserver = {
-      enable = true;
-      displayManager.gdm = {
+
+  gnomePackages = with pkgs.gnome; [
+    adwaita-icon-theme
+    gnome-tweaks
+    pomodoro
+  ];
+  
+in {
+  config = {
+    environment.systemPackages = with pkgs; [
+      dconf2nix
+    ] ++ gnomePackages ++ extensions;
+    programs = {
+      dconf.enable = true;
+      kdeconnect = {
         enable = true;
-	wayland = true;
-	debug = true;
-      };  
-      desktopManager.gnome.enable = true;
-      libinput.enable = true;
+        package = pkgs.gnomeExtensions.gsconnect;
+      };
+    # gdm wont find user if `users.users.<user>.shell = pkgs.zsh;` and this isn't set
+      zsh.enable = true;
     };
-    udev.packages = with pkgs; [ gnome.gnome-settings-daemon ];
+    services = {
+      gnome = {
+        gnome-keyring.enable = true;
+        sushi.enable = true;
+        gnome-initial-setup.enable = false;
+      };
+      xserver = {
+        enable = true;
+        displayManager.gdm = {
+          enable = true;
+          #wayland = true;
+          #debug = true;
+        };  
+        desktopManager.gnome.enable = true;
+        libinput.enable = true;
+      };
+      udev.packages = with pkgs; [ gnome.gnome-settings-daemon ];
+    };
+    security.pam.services.gdm.enableGnomeKeyring = true;
   };
-  security.pam.services.gdm.enableGnomeKeyring = true;
-};
 }
 
-/*
-
-ICEAutFile
-unable to open named profile (/dconf/user)
-Warning IceLockAuthFile failed: Permission denied
-*/
