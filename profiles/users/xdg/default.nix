640b9821f9c3452359ed85b7f926efd837ff651e
Th3Whit3Wolf <the.white.wolf.is.1337@gmail.com>
2021-12-11 15:49:32 +0000

    Fixed paths

--- profiles/users/xdg/default.nix
+++ profiles/users/xdg/default.nix
@@ -1,26 +1,26 @@
-{
+{ config, lib, pkgs, ... }:{
   systemd.user.sessionVariables = {
     # Force XDG Compliance
-    ACKRC = "$XDG_CONFIG_HOME/ack/ackrc";
-    #DCONF_PROFILE = "$XDG_CONFIG_HOME/dconf/user";
-    ELINKS_CONFDIR = "$XDG_CONFIG_HOME/elinks";
+    ACKRC = "${config.xdg.configHome}/ack/ackrc";
+    #DCONF_PROFILE = "${config.xdg.configHome}/dconf/user";
+    ELINKS_CONFDIR = "${config.xdg.configHome}/elinks";
     MANPAGER = "sh -c 'col -bx | bat --paging=always -l man -p'";
     PAGER = "less";
-    ELECTRUMDIR = "$XDG_DATA_HOME/electrum";
+    ELECTRUMDIR = "${config.xdg.dataHome}/electrum";
     BAT_PAGER = "less -RFXi";
-    LESSKEY = "$XDG_CONFIG_HOME/less/lesskey";
-    LESSHISTFILE = "$XDG_CACHE_HOME/less/history";
-    MOST_INITFILE = "$XDG_CONFIG_HOME/mostrc";
-    GRIPHOME = "$XDG_CONFIG_HOME/grip";
-    MPLAYER_HOME = "$XDG_CONFIG_HOME/mplayer";
-    TERMINFO = "$XDG_DATA_HOME/terminfo";
-    TERMINFO_DIRS = "$XDG_DATA_HOME/terminfo:/usr/share/terminfo";
-    NOTMUCH_CONFIG = "$XDG_CONFIG_HOME/notmuch/config";
-    PARALLEL_HOME = "$XDG_CONFIG_HOME/parallel";
-    #ICEAUTHORITY = "$XDG_CACHE_HOME/ICEauthority";
-    IMAPFILTER_HOME = "$XDG_CONFIG_HOME/imapfilter";
-    INPUTRC = "$XDG_CONFIG_HOME/readline/inputrc";
-    SCREENRC = "$XDG_CONFIG_HOME/screen/screenrc";
-    WEECHAT_HOME = "$XDG_CONFIG_HOME/weechat";
+    LESSKEY = "${config.xdg.configHome}/less/lesskey";
+    LESSHISTFILE = "${config.xdg.cacheHome}/less/history";
+    MOST_INITFILE = "${config.xdg.configHome}/mostrc";
+    GRIPHOME = "${config.xdg.configHome}/grip";
+    MPLAYER_HOME = "${config.xdg.configHome}/mplayer";
+    TERMINFO = "${config.xdg.dataHome}/terminfo";
+    TERMINFO_DIRS = "${config.xdg.dataHome}/terminfo:/usr/share/terminfo";
+    NOTMUCH_CONFIG = "${config.xdg.configHome}/notmuch/config";
+    PARALLEL_HOME = "${config.xdg.configHome}/parallel";
+    #ICEAUTHORITY = "${config.xdg.cacheHome}/ICEauthority";
+    IMAPFILTER_HOME = "${config.xdg.configHome}/imapfilter";
+    INPUTRC = "${config.xdg.configHome}/readline/inputrc";
+    SCREENRC = "${config.xdg.configHome}/screen/screenrc";
+    WEECHAT_HOME = "${config.xdg.configHome}/weechat";
   };
 }
