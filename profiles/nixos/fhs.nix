88f394a34da7d5f732a955addf126ff944c44e21
Th3Whit3Wolf <the.white.wolf.is.1337@gmail.com>
2021-12-11 15:50:32 +0000

    Made profile for fhs

--- profiles/nixos/fhs.nix
+++ profiles/nixos/fhs.nix
@@ -9,8 +9,17 @@
         };
         lsb = {
             enable = true;
-            enableDesktop = true;
+            #enableDesktop = true;
             support32Bit = true;
         };
+        sessionVariables.LD_LIBRARY_PATH = lib.mkForce "/lib";
+        systemPackages = with pkgs; [
+            pkg-config
+            openssl
+            openssl.dev
+            freetype
+            fontconfig
+            expat
+        ];
     };
 }
