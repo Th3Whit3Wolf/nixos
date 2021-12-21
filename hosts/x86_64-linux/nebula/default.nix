448c6099c743716c96d51c980f00e55dbbb3ee68
Th3Whit3Wolf <the.white.wolf.is.1337@gmail.com>
2021-12-21 15:54:55 +0000

    Remove console

--- hosts/x86_64-linux/nebula/default.nix
+++ hosts/x86_64-linux/nebula/default.nix
@@ -15,10 +15,6 @@ in {
   time.timeZone = "Europe/London";
   # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
-  console = {
-    font = "Lat2-Terminus16";
-    keyMap = "us";
-  };
 
   # Enable sound.
   sound.enable = true;
