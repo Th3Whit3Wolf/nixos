6fa73b2a73e1ce88d7486b087169b299448450a9
Th3Whit3Wolf <the.white.wolf.is.1337@gmail.com>
2021-12-21 15:26:42 +0000

    Added eyd support for chromium

--- modules/users/services/eyd/default.nix
+++ modules/users/services/eyd/default.nix
@@ -11,6 +11,10 @@ let
         ${pkgs.rsync}/bin/rsync -avh --exclude={'user.js','chrome','extensions','lock'} /home/${username}/.mozilla/firefox/${username} ${cfg.persistentPath}/home/${username}/.mozilla/firefox/
     '');
 
+    chromium-backup = (pkgs.writeShellScript "chromium-backup" ''
+        ${pkgs.rsync}/bin/rsync -avh  /home/${username}/.config/chromium ${cfg.persistentPath}/home/${username}/.config/chromium/
+    '');
+
 in {
     options = {
         services.eyd = {
@@ -30,14 +34,23 @@ in {
                 '';
                 type = types.bool;
             };
+
+            chromium = mkOption {
+                default = false;
+                description = ''
+                    Automatically sync chromium.
+                '';
+                type = types.bool;
+            };
+
         };
     };
 
     config = mkIf cfg.enable {
-        systemd.user = mkIf (cfg.firefox) {
+        systemd.user = mkIf (cfg.firefox || cfg.chromium) {
             # This is effectively what psd does
             services = {
-                firefox-persist = {
+                firefox-persist = mkIf cfg.firefox {
                     Unit = {
                         Description = "Firefox persistent storage sync";
                         Before = "shutdown.target";
@@ -52,7 +65,7 @@ in {
                     };
                 };
 
-                firefox-persist-pre-shutdown = {
+                firefox-persist-pre-shutdown = mkIf cfg.firefox {
                     Unit = {
                         Description = "Firefox persistent storage sync before shutdown";
                     };
@@ -67,7 +80,7 @@ in {
                     Install = { WantedBy = [ "shutdown.target" ]; };
                 };
                 
-                firefox-persist-login-logout = {
+                firefox-persist-login-logout = mkIf cfg.firefox {
                     Unit = {
                         Description = "Firefox persistent storage sync on login/logout";
                     };
@@ -87,13 +100,79 @@ in {
                 };
             };
 
-            timers.firefox-persist = {
+            timers.firefox-persist = mkIf cfg.firefox {
                 Unit = {
                     Description = "Firefox persistent storage periodic sync";
                 };
 
                 Timer = {
                     Unit = "firefox-persist.service";
+                    # Run every 15 minutes
+                    OnCalendar = "*:0/15";
+                    Persistent = true;
+                };
+
+                Install = { WantedBy = [ "timers.target" ]; };
+            };
+
+            services = {
+                chromium-persist = mkIf cfg.chromium {
+                    Unit = {
+                        Description = "Chromium persistent storage sync";
+                        Before = "shutdown.target";
+                    };
+
+                    Service = {
+                        CPUSchedulingPolicy = "idle";
+                        IOSchedulingClass = "idle";
+                        Environment = "PATH=${pkgs.coreutils}";
+                        # copy all files except those that are symlinks
+                        ExecStart = toString chromium-backup;
+                    };
+                };
+
+                chromium-persist-pre-shutdown = mkIf cfg.chromium {
+                    Unit = {
+                        Description = "Chromium persistent storage sync before shutdown";
+                    };
+
+                    Service = {
+                        Environment = "PATH=${pkgs.coreutils}";
+                        # copy all files except those that are symlinks
+                        ExecStart = toString chromium-backup;
+                        TimeoutStartSec = 0;
+                    };
+
+                    Install = { WantedBy = [ "shutdown.target" ]; };
+                };
+                
+                chromium-persist-login-logout = mkIf cfg.chromium {
+                    Unit = {
+                        Description = "Chromium persistent storage sync on login/logout";
+                    };
+
+                    Service = {
+                        Type = "oneshot";
+                        Environment = "PATH=${pkgs.coreutils}";
+                        RemainAfterExit = true;
+                        StandardOutput = "journal";
+                        # copy all files except those that are symlinks
+                        ExecStart = "${pkgs.coreutils}/bin/cp -r ${cfg.persistentPath}/home/${username}/.config/chromium /home/${username}/.config";
+                        ExecStop = toString chromium-backup;
+                        TimeoutStartSec = 0;
+                    };
+
+                    Install = { WantedBy = [ "default.target" ]; };
+                };
+            };
+
+            timers.chromium-persist = mkIf cfg.chromium {
+                Unit = {
+                    Description = "Chromium persistent storage periodic sync";
+                };
+
+                Timer = {
+                    Unit = "chromium-persist.service";
                     # Run every 15 minutes
                     OnCalendar = "*:0/15";
                     Persistent = true;
